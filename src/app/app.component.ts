5f275846e99313e2d88a6e196835e104a6ffac32
Cristache Andrei Razvan <cristache.razvan99@gmail.com>
2021-04-11 02:16:06 +0300

    added register form and functionallity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +12,20 @@ export class AppComponent implements OnInit {
   title = 'The dating APP';
   users:any;
 
-  constructor(private http:HttpClient )
+  constructor(private accountSerive:AccountService )
   {
 
   }
   ngOnInit(){
-    this.getUsers();
+
+    this.setCurrentUser();
   }
-  getUsers()
-  {
-    this.http.get('https://localhost:44399/api/users').subscribe(response=>{
-      this.users=response;
-    },error=>{
-      console.log(error);
-    })
-  }
+
+setCurrentUser()
+{
+  const user:User=JSON.parse(localStorage.getItem('user'));
+  this.accountSerive.setCurrentUser(user);
+}
+
+
 }
