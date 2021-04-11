21aef44ff58012c467dac02544ce5fb12dba069d
Cristache Andrei Razvan <cristache.razvan99@gmail.com>
2021-04-11 19:12:01 +0300

    s6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,24 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { SharedModule } from './_modules/shared.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -26,7 +37,7 @@ import { RegisterComponent } from './register/register.component';
     BrowserAnimationsModule,
     FormsModule,
     BsDropdownModule,
-    BsDropdownModule.forRoot()
+    SharedModule
 
   ],
   providers: [],
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -10,7 +12,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
 model: any={};//initialize empty object
-  constructor(public accountService:AccountService) { }
+  constructor(public accountService:AccountService,private router:Router,private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -20,15 +22,18 @@ model: any={};//initialize empty object
   login()
   {
     this.accountService.login(this.model).subscribe(response=>{
-      console.log(response);
+     this.router.navigateByUrl('/members');
     },error=>{
       console.log(error);
+      this.toastr.error(error.error);
+
     })
   }
 
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
 
