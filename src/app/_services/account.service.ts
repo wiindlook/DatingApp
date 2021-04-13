3902cb5460f40bc4db3f4d4a2cb07b9ee8160cdd
wiindlook <razvy_boy2011@yahoo.com>
2021-04-13 17:30:16 +0300

    s9 updating user interface

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import {map} from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({     //decoradul =>   //acest serviciu poate fi injectat in alte componente sau alte servicii
   providedIn: 'root'  //un serviciu in angular este singleton ramane activ mereu.
 })
 export class AccountService {
-  baseUrl='https://localhost:44399/api/';
+  baseUrl=environment.apiUrl;
   private currentUserSource=new ReplaySubject <User>(1);
   currentUser$=this.currentUserSource.asObservable();
 
